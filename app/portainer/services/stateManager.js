76d462fddfef09274f6881fef7c28cac000ce44a
waysonwei <degui.wei@gmail.com>
2021-07-26 16:18:53 +1200

    backport from EE to CE

--- app/portainer/services/stateManager.js
+++ app/portainer/services/stateManager.js
@@ -67,6 +67,11 @@ angular.module('portainer.app').factory('StateManager', [
       LocalStorage.storeApplicationState(state.application);
     };
 
+    manager.updateTheme = function (theme) {
+      state.application.theme = theme;
+      LocalStorage.storeApplicationState(state.application);
+    };
+
     manager.updateSnapshotInterval = function (interval) {
       state.application.snapshotInterval = interval;
       LocalStorage.storeApplicationState(state.application);
